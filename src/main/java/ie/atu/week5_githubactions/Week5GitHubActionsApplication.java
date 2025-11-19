044d67ecee78fe0fd50422fce21f4bcf0b7bf09f
Donal1999 <g00365724@atu.ie>
2025-11-19 20:31:57 +0000

    created project

---
+++ src/main/java/ie/atu/week5_githubactions/Week5GitHubActionsApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.week5_githubactions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week5GitHubActionsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Week5GitHubActionsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/week5_githubactions/Week5GitHubActionsApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.week5_githubactions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week5GitHubActionsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
