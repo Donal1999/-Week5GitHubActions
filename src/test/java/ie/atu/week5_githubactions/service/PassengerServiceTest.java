c2a246a02c5039752748a8241a18276c8ecbc246
Donal1999 <g00365724@atu.ie>
2025-11-20 19:17:44 +0000

    added duplicate test

--- src/test/java/ie/atu/week5_githubactions/service/PassengerServiceTest.java
+++ src/test/java/ie/atu/week5_githubactions/service/PassengerServiceTest.java
@@ -6,8 +6,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class PassengerServiceTest {
     private PassengerService service;
@@ -24,5 +23,13 @@ public class PassengerServiceTest {
         Optional<Passenger> found = service.findById("id1");
         assertTrue(found.isPresent());
         assertEquals("Adam", found.get().getName());
+    }
+
+    @Test
+    void duplicateIdThrows(){
+        Passenger p = new Passenger("id2","Bob", "bob@atu.ie");
+        Passenger p2= new Passenger("id2","Adam", "adam@atu.ie");
+        service.create(p);
+        assertThrows(IllegalArgumentException.class, () -> service.create(p2));
     }
 }
